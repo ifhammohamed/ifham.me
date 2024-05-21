9509adfca0b1b6918d67561f14abfbe45ec4172a
Ifham <ifhammohamed1111@gmail.com>
2024-05-21 21:02:44 +0530

    Update Bio component with new name and roles

--- src/data/const.js
+++ src/data/const.js
@@ -1,17 +1,16 @@
 export const Bio = {
-  name: "Rishav Chanda",
+  name: "Ifham Mohamed",
   roles: [
     "Full Stack Developer",
-    "Android Developer",
-    "UI/UX Designer",
-    "Programmer",
+    "UI/UX Enthusiast",
+    "Open Source Contributor",
   ],
   description:
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
-  github: "https://github.com/rishavchanda",
+  github: "https://github.com/ifhammohamed1111",
   resume:
-    "https://drive.google.com/file/d/1ffZrcMcn8UatXGIaautbbqpV7ADNaETA/view?usp=sharing",
-  linkedin: "https://www.linkedin.com/in/rishav-chanda-b89a791b3/",
+    "https://drive.google.com/drive/folders/1UCnfVTUApFqrytKdY4MuFl2OFSYZEyjh?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/ifhammohamed1111/",
   twitter: "https://twitter.com/RishavChanda",
   insta: "https://www.instagram.com/rishav_chanda/",
   facebook: "https://www.facebook.com/rishav.chanda.165/",

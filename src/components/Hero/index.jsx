2d358d66035eee1fb2519d65b9d86a47d4c15be7
Ifham <ifhammohamed1111@gmail.com>
2024-05-21 20:47:56 +0530

    chore: Add Hero component and styles for the landing page

---
+++ src/components/Hero/index.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import HeroBgAnimation from '../HeroBgAnimation'
+import Typewriter from 'typewriter-effect';
+import { Bio } from '../../data/const';
+import HeroImg from '../../images/HeroImage.jpg';
+import { HeroBg, HeroContainer, HeroInnerContainer, HeroLeftContainer, HeroRightContainer, Img, ResumeButton, Span, SubTitle, TextLoop, Title } from './HeroStyle';
+
+function Hero() {
+    return (
+        <div id="about">
+            <HeroContainer>
+                <HeroBg>
+                    <HeroBgAnimation />
+                </HeroBg>
+                <HeroInnerContainer >
+                    <HeroLeftContainer id="Left">
+                        <Title>Hi, I am <br /> {Bio.name}</Title>
+                        <TextLoop>
+                            I am a
+                            <Span>
+                                <Typewriter
+                                    options={{
+                                        strings: Bio.roles,
+                                        autoStart: true,
+                                        loop: true,
+                                    }}
+                                />
+                            </Span>
+                        </TextLoop>
+                        <SubTitle>{Bio.description}</SubTitle>
+                        <ResumeButton href={Bio.resume} target='_blank'>Check Resume</ResumeButton>
+                    </HeroLeftContainer>
+
+                    <HeroRightContainer id="Right">
+
+                        <Img src={HeroImg} alt="hero-image" />
+                    </HeroRightContainer>
+                </HeroInnerContainer>
+
+            </HeroContainer>
+            <p>hi</p>
+        </div>
+    )
+}
+
+export default Hero

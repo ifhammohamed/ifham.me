e2c8ed9b6e9b1ccafa9deed1f22f3e3642303f02
Ifham <ifhammohamed1111@gmail.com>
2024-05-22 05:16:00 +0530

    feat: Add Contact section to Footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -85,30 +85,32 @@ const Copyright = styled.p`
 `;
 
 function Footer() {
-    return (
-        <FooterContainer>
-            <FooterWrapper>
-                <Logo>Ifham Mohamed</Logo>
-                <Nav>
-                    <NavLink href="#about">About</NavLink>
-                    <NavLink href="#skills">Skills</NavLink>
-                    <NavLink href="#experience">Experience</NavLink>
-                    <NavLink href="#projects">Projects</NavLink>
-                    <NavLink href="#education">Education</NavLink>
-                </Nav>
-                <SocialMediaIcons>
-                    <SocialMediaIcon href={Bio.facebook} target="display"><FacebookIcon /></SocialMediaIcon>
-                    <SocialMediaIcon href={Bio.twitter} target="display"><TwitterIcon /></SocialMediaIcon>
-                    <SocialMediaIcon href={Bio.linkedin} target="display"><LinkedInIcon /></SocialMediaIcon>
-                    <SocialMediaIcon href={Bio.insta} target="display"><InstagramIcon /></SocialMediaIcon>
-                </SocialMediaIcons>
-                <Copyright>
-                    &copy; 2024 Ifham Mohamed. All rights reserved.
-                </Copyright>
+  return (
+    <FooterContainer>
+      <FooterWrapper>
+        <Logo>Ifham Mohamed</Logo>
+        <Nav>
+          <NavLink href="#about">About</NavLink>
+          <NavLink href="#skills">Skills</NavLink>
+          <NavLink href="#experience">Experience</NavLink>
+          <NavLink href="#projects">Projects</NavLink>
+          <NavLink href="#education">Education</NavLink>
+          <NavLink href='#contact'>Contact</NavLink>
 
-            </FooterWrapper>
-        </FooterContainer>
-    );
+        </Nav>
+        <SocialMediaIcons>
+          <SocialMediaIcon href={Bio.facebook} target="display"><FacebookIcon /></SocialMediaIcon>
+          <SocialMediaIcon href={Bio.twitter} target="display"><TwitterIcon /></SocialMediaIcon>
+          <SocialMediaIcon href={Bio.linkedin} target="display"><LinkedInIcon /></SocialMediaIcon>
+          <SocialMediaIcon href={Bio.insta} target="display"><InstagramIcon /></SocialMediaIcon>
+        </SocialMediaIcons>
+        <Copyright>
+          &copy; 2024 Ifham Mohamed. All rights reserved.
+        </Copyright>
+
+      </FooterWrapper>
+    </FooterContainer>
+  );
 }
 
 export default Footer;

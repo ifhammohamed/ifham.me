8eb9cd2a443f2b46a6319e65c34d2453eb02a51e
ifham-IM <ifhammohamed1111@gmail.com>
2024-10-02 21:44:48 +0530

    project deploy vonfig changed

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/ifham.me/",
+  base: "./",
   plugins: [react()],
+  build: {
+    outDir: "build", // You can use 'build' or stick with 'dist'
+  },
 });
